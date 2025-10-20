27be9fa5005d542186b970fa170f6a1f2957e817
lucieDb <lucie.debrabandere@gmail.com>
2025-10-20 10:49:54 +0200

    remove the useless details

--- app/errors/conversion_error.rb
+++ app/errors/conversion_error.rb
@@ -1,10 +1,9 @@
 # Custom error used when a conversion operation fails for known reasons.
 class ConversionError < StandardError
-  attr_reader :reason, :details
+  attr_reader :reason
 
-  def initialize(reason, details = {})
+  def initialize(reason)
     @reason = reason.to_sym
-    @details = details
     super(reason.to_s)
   end
 end
