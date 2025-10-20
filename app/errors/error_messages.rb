d4b3483f6d17ed75cde10143c5739eee370882b3
lucieDb <lucie.debrabandere@gmail.com>
2025-10-20 10:50:07 +0200

    syntax

--- app/errors/error_messages.rb
+++ app/errors/error_messages.rb
@@ -1,7 +1,7 @@
 # Error messages dictionnary : centralized error messages for conversions
 module ErrorMessages
   CONVERSION = {
-    input_value_not_numeric: "Input value must be a valid number",
-    units_incompatible: "Source and target units are not compatible"
+    input_value_not_numeric: "Input value must be a valid number.",
+    units_incompatible: "Source and target units are not compatible."
   }.freeze
 end
